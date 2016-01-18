ff6370ea4ead2da7ea7d12ec06ac68da19fcee62
Yorkfine Chan <yorkfinechan@gmail.com>
2016-01-19 05:15:28 +1100

    Modified: refactora SettingsFragment UI

--- app/src/main/java/com/itime/team/itime/fragments/SettingsFragment.java
+++ app/src/main/java/com/itime/team/itime/fragments/SettingsFragment.java
@@ -17,65 +17,27 @@
 package com.itime.team.itime.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.itime.team.itime.activities.R;
 
 /**
  * Created by Xuhui Chen (yorkfine) on 10/01/16.
  */
-public class SettingsFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener{
+public class SettingsFragment extends Fragment {
 
+    @Nullable
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        // Set the default white background in the view so as to avoid transparency
-        view.setBackgroundColor(
-                ContextCompat.getColor(getContext(), R.color.background_material_light));
-
-    }
-
-    @Override
-    public void onCreatePreferences(Bundle bundle, String s) {
-        //addPreferencesFromResource(R.xml.preferences);
-        setPreferencesFromResource(R.xml.preferences, s);
-        bindPreferenceSummaryToValue(findPreference("name"));
-        bindPreferenceSummaryToValue(findPreference("iTime_id"));
-        bindPreferenceSummaryToValue(findPreference("email"));
-        bindPreferenceSummaryToValue(findPreference("phone"));
-    }
-
-
-/**
-     * Attaches a listener so the summary is always updated with the preference value.
-     * Also fires the listener once, to initialize the summary (so it shows up before the value
-     * is changed.)
-     */
-    private void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(this);
-
-        // Trigger the listener immediately with the preference's
-        // current value.
-        onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object o) {
-        String value = o.toString();
-
-        if (preference instanceof EditTextPreference) {
-            preference.setSummary(value);
-        }
-        return true;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }

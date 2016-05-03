a4f2f0bc6d58db933341ba1679166d864222e8fe
yinchuandong <yincd520@sina.com>
2016-05-03 11:50:39 +1000

    modified MeetingScrollView to deal with clicking on blank area, but I did not add listener, it is up to you

--- app/src/main/java/com/itime/team/itime/views/MeetingScrollView.java
+++ app/src/main/java/com/itime/team/itime/views/MeetingScrollView.java
@@ -2,11 +2,16 @@ package com.itime.team.itime.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 import com.itime.team.itime.listener.ScrollMeetingViewListener;
 import com.itime.team.itime.listener.ScrollViewInterceptTouchListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by mac on 16/3/7.
@@ -14,6 +19,8 @@ import com.itime.team.itime.listener.ScrollViewInterceptTouchListener;
 public class MeetingScrollView extends ScrollView {
     private ScrollMeetingViewListener scrollViewListener = null;
     private ScrollViewInterceptTouchListener scrollViewInterceptTouchListener = null;
+
+    private final static String TAG = "MeetingScrollView";
 
     public MeetingScrollView(Context context) {
         super(context);
@@ -49,6 +56,33 @@ public class MeetingScrollView extends ScrollView {
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         if (scrollViewInterceptTouchListener != null) {
             scrollViewInterceptTouchListener.touchEventHappend(this, ev);
+        }
+
+        if (ev.getAction() == MotionEvent.ACTION_UP){
+            Log.d(TAG, ev.getAction() + "----");
+            RelativeLayout subLayout = (RelativeLayout)this.getChildAt(0);
+            int subCount = subLayout.getChildCount();
+            ArrayList<CustomizedTextView> viewList = new ArrayList<CustomizedTextView>();
+            for(int i = 0; i < subCount; i++){
+                View textView = subLayout.getChildAt(i);
+                if (textView instanceof CustomizedTextView){
+                    viewList.add((CustomizedTextView)textView);
+                }
+            }
+            int y = (int)ev.getY();
+            for (CustomizedTextView textView : viewList){
+                int top = textView.getTop();
+                int bottom = textView.getBottom();
+                int sy = this.getScrollY();
+                int pointY = y + sy;
+                if (top <= pointY && pointY <= bottom){
+                    // if pointer is in event area, deal with text view
+                    return super.onInterceptTouchEvent(ev);
+                }
+            }
+            // if pointer is not int event area, deal with scroll view
+            // add an listener here to handle blank area click event
+            return true;
         }
 
         return super.onInterceptTouchEvent(ev);
